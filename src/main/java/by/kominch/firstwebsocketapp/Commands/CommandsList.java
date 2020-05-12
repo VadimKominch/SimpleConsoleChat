bf1e2220a62c5fc060bfab4e460d2e557650dab0
VadimKominch <kominch.vadim@yandex.ru>
2020-05-12 12:30:23 +0300

    Add base commands list

---
+++ src/main/java/by/kominch/firstwebsocketapp/Commands/CommandsList.java
@@ -0,0 +1,8 @@
+public class CommandsList {
+	public static final String ENTER_THE_CHAT = "/enter";
+	public static final String LEAVE_THE_CHAT = "/leave";
+	public static final String REGISTER_THE_CHAT = "/register";
+	
+	
+	
+}
